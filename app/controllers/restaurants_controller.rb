f6e7e5cd2a99b0e762c1b6437f5b7267c4f62cc2
Stefan Wawrzyn <swawrzyn@gmail.com>
2018-11-14 14:50:08 +0800

    made it kinda pretty

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -6,6 +6,7 @@ class RestaurantsController < ApplicationController
   def show
     @restaurant = Restaurant.find(params[:id])
     @reviews = Review.where(restaurant: @restaurant).reverse
+    @review = Review.new
   end
 
   def new
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -5,4 +5,13 @@ class Restaurant < ApplicationRecord
   validates :name, presence: true
   validates :address, presence: true
   validates :category, inclusion: { in: %w[chinese italian japanese french belgian] }
+
+  def average_rating
+    average = 0
+    return average if reviews.count.zero?
+
+    average.to_f
+    reviews.each { |review| average += review.rating }
+    (average / reviews.count).floor
+  end
 end
